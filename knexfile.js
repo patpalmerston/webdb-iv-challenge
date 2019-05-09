b1ba52506e3b2e8966eaa93ca294f5998f875498
patpalmerston <patpalmerston@gmail.com>
2019-05-09 15:53:24 -0700

    pool functional now

--- knexfile.js
+++ knexfile.js
@@ -14,43 +14,12 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     conn.run(('PRAGMA foreign_keys = ON', done))
-    //   }
-    // }
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
+    }
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
